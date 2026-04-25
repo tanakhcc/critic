ecedb69a7572a8ba831d3029b0824ac39a6dacef
Jonathan Schleucher <mathnathan@icloud.com>
2026-04-25 15:52:13 +0200

    changed schema for transcriptions and reconciliations

--- critic-db/migrations/20260425155001_transcription.up.sql
+++ critic-db/migrations/20260425155001_transcription.up.sql
@@ -2,13 +2,14 @@
 --- a transcription is basically a blob of TEI/XML data for an individual page of a manuscript
 CREATE TABLE transcription (
 	id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-	--- the page this transcription is for (also includes information about the manuscript)
-	page BIGINT NOT NULL REFERENCES page(id),
+	--- the baseline this transcription is for
+	line BIGINT NOT NULL REFERENCES page(id),
 	--- does the user wish this transcription to be viewable
 	published BOOL NOT NULL DEFAULT false,
 	--- the name of the user who created this transcription
 	username TEXT NOT NULL REFERENCES user_session(username),
+	--- the actual xml data parsable with the `page_from_xml` function
+	content TEXT NOT NULL,
 	--- only one version for a page and a user is saved
-	UNIQUE(page, username)
-	--- the actual xml data resides on disk at `data_directory`/transcript/manuscript/page/user.tei.xml
+	UNIQUE(line, username)
 );
--- critic-db/migrations/20260425155101_reconciliation.up.sql
+++ critic-db/migrations/20260425155101_reconciliation.up.sql
@@ -5,5 +5,8 @@ CREATE TABLE reconciliation (
 	--- each page may only be reconciled once
 	page BIGINT UNIQUE NOT NULL REFERENCES page(id),
 	--- the name of the user who created this reconciliation
-	username TEXT NOT NULL REFERENCES user_session(username)
+	username TEXT NOT NULL REFERENCES user_session(username),
+	--- the actual content in this reconciliation - this is a blob of TEI XML
+	--- parsable with the page_from_xml function
+	content TEXT NOT NULL
 );
